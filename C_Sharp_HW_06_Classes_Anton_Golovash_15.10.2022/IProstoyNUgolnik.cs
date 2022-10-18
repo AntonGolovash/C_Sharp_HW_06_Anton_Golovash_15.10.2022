4512fcc4d4fd42cd200cddf1ba885a4bbda9444c
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-10-18 20:56:49 +0300

    0

---
+++ C_Sharp_HW_06_Classes_Anton_Golovash_15.10.2022/IProstoyNUgolnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_HW_06_Classes_Anton_Golovash_15._10._2022
+{
+    public interface IProstoyNUgolnik
+    {
+        double Vysota();
+        double Osnovanie();
+        double UgolMeghduOsnovaniemISmeghnoyStoronoy();
+        int KolichestvoStoron();
+        double DlinyStoron();
+        double Ploshad();
+        double Perimetr();
+    }
+}
---
+++ C_Sharp_HW_06_Classes_Anton_Golovash_15.10.2022/SostvnayaFigura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_HW_06_Classes_Anton_Golovash_15._10._2022
+{
+    public class SostvnayaFigura : IProstoyNUgolnik
+    {
+        public double DlinyStoron()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int KolichestvoStoron()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Osnovanie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Perimetr()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Ploshad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double UgolMeghduOsnovaniemISmeghnoyStoronoy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Vysota()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
