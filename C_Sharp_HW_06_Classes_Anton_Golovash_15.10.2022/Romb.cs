6c4972e928c7139acaf9d22d4bd6da3a70d78738
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-10-21 15:21:36 +0300

    1

--- C_Sharp_HW_06_Classes_Anton_Golovash_15.10.2022/Romb.cs
+++ C_Sharp_HW_06_Classes_Anton_Golovash_15.10.2022/Romb.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_HW_06_Classes_Anton_Golovash_15._10._2022
 {
-    public class Romb : GeometricheskayaFigura
+    public class Romb : GeometricheskayaFigura, IProstoyNUgolnik
     {
+        public double DlinyStoron()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int KolichestvoStoron()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Osnovanie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Perimetr()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Ploshad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double UgolMeghduOsnovaniemISmeghnoyStoronoy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Vysota()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
